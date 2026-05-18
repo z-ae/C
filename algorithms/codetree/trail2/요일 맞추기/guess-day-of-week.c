283d8f81f2902257e4555ba0f240250dfcee0b3b
Jeong Ji Ae <jiea031225@gmail.com>
2026-05-18 20:22:37 +0900

    [Passed] 요일 맞추기 (8ms, 0MB)

---
+++ algorithms/codetree/trail2/요일 맞추기/guess-day-of-week.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_days(int month, int day){
+    int day_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int days = day;
+
+    for (int i = 0; i < month - 1; i++){
+        days += day_in_month[i];
+    }
+    return days;
+}
+int main() {
+    // Please write your code here.
+    int m1, m2, d1, d2;
+    char day_name[7][4] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    scanf("%d %d %d %d", &m1, &d1, &m2, &d2);
+
+    // 몇 일 차이나는지
+    int base_days =  get_days(m1, d1);
+    int target_days =  get_days(m2, d2);
+    int diff_days = target_days - base_days;
+
+    // 요일 점프
+    if (diff_days >= 0){
+        printf("%s", day_name[diff_days % 7]);
+    }
+    else{
+        printf("%s", day_name[7 + diff_days % 7]);
+    }
+
+    return 0;
+}
