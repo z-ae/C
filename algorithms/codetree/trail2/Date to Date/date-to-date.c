39c400f5bcf6c8e84b58e4bf0d52130a46578a2e
Jeong Ji Ae <jiea031225@gmail.com>
2026-05-18 17:18:31 +0900

    [Wrong Answer] Date to Date (8ms, 0MB)

--- algorithms/codetree/trail2/Date to Date/date-to-date.c
+++ algorithms/codetree/trail2/Date to Date/date-to-date.c
@@ -10,13 +10,13 @@ int main() {
     scanf("%d %d %d %d ", &m1, &d1, &m2, &d2);
 
     days_1 = d1; days_2 = d2;
-    for (int i = 0; i < m1 - 1; i++){
+    for (int i = 0; i < m1; i++){
         days_1 += days_of_month[i];
     }
-    for (int i = 0; i < m2 - 1; i++){
+    for (int i = 0; i < m2; i++){
         days_2 += days_of_month[i];
     }
 
-    printf("%d", days_2 - days_1);
+    printf("%d", days_2 - days_1 - 1);
     return 0;
 }
